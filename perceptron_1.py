30b4a8a71ff1bbc46f72f75d452d313ee2f0356d
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2016-04-21 12:50:03 +0900

    perceptron_1のファイルを追加する

---
+++ perceptron_1.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 10:47:37 2016
+
+@author: matsumi
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+
+
+# データセットを読み込む
+digits = load_digits()
+images = digits.images
+T = digits.target
+X = digits.data
+print T.shape
+print X.shape
+print digits.data.shape
+plt.matshow(images[0], cmap=plt.cm.gray)
+
+# 学習率を定義する
+rho = 0.5
+
+# wを定義する
+w = np.random.randn(64)
+
+# 最大のループ回数を定義する
+max_iteration = 100
+
+# 一番大きなループ
+for i in range(max_iteration):
+    print i
+
+    # 識別関数の値を計算する
+    for (x_i, t_i) in zip(X, T):
+        g_i = np.inner(w, x_i)
+        # wを更新する
+        # wの更新が良ければ更新しない
+
+# 予測クラスと比較する
+
+# wを表示する
