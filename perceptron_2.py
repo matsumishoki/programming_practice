25a58befe57ebb9e15593c9f4913ea4fedfe86fc
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2016-04-21 17:05:44 +0900

    スケッチを追加する

--- perceptron_2.py
+++ perceptron_2.py
@@ -8,3 +8,19 @@ Created on Thu Apr 21 16:54:08 2016
 import numpy as np
 import matplotlib as plt
 from sklearn.datasets import load_digits
+
+# データセットを読み込む
+
+# wを定義する
+
+# 学習率(rho)を定義する
+
+# 外側のループ
+
+    # 学習させるループ
+
+    # wの修正条件
+
+# 予測クラスと正解クラスの値を比較する
+
+# wの可視化
