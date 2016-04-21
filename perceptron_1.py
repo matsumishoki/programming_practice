8e56c54405d754c68351de5e433ad3d31a642606
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2016-04-21 15:39:59 +0900

    予測クラスと正解ラベルと比較し，正解率を表示する

--- perceptron_1.py
+++ perceptron_1.py
@@ -11,10 +11,11 @@ from sklearn.datasets import load_digits
 
 
 # データセットを読み込む
-digits = load_digits()
+digits = load_digits(2)
 images = digits.images
 T = digits.target
 X = digits.data
+num_examples = len(X)
 print T.shape
 print X.shape
 print digits.data.shape
@@ -31,20 +32,23 @@ max_iteration = 100
 
 # 一番大きなループ
 for i in range(max_iteration):
-    print i
+   # print i
 
     # 識別関数の値を計算する
     for (x_i, t_i) in zip(X, T):
         g_i = np.inner(w, x_i)
         # wを更新する
         if t_i * g_i < 0:
+            t_i = np.sign(t_i)
             w_new = w + rho * t_i * x_i
-            print t_i
     # wの更新が良ければ更新しない
-    else:
-        w_new = w
-    w = w_new
+        else:
+            w_new = w
+        w = w_new
 
-# 予測クラスと比較する
-
+    # 予測クラスと比較する
+    y = np.sign(np.inner(w, X))
+    num_correct = np.sum(y == T)
+    correct_accuracy = num_correct / float(num_examples) * 100
+    print correct_accuracy
 # wを表示する
