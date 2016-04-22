da1e1611adf0661575905c2e9bd86d1920842b27
matsumi shoki <is0188ee@ed.ritsumei.ac.jp>
2016-04-22 13:12:06 +0900

    perceptron_3のファイルを追加する

---
+++ perceptron_3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 13:08:48 2016
+
+@author: matsumi
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+
